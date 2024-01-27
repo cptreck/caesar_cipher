d3889ab5fcc2ce5dd8983f07c8f437ee68e9faf6
Jesse Caccavale <58094016+cptreck@users.noreply.github.com>
2024-01-26 22:16:06 -0500

    finish caesar_cipher method

--- cipher.rb
+++ cipher.rb
@@ -6,26 +6,29 @@
 # find character in alphabet and apply number, 
 # if z start back at a
 
-# def caesar_cipher (str, num)
+$alpha = "abcdefghijklmnopqrstuvwxyz"
 
-
-# end
-
-# def loop_alpha (char, num)
-#     alpha = "abcdefghijklmnopqrstuvwxyz"
-
-#     alpha.each_char do |letter| 
-#         if letter == char.downcase
-#             new_index = alpha[letter.index] + num 
-#             alpha.index[new_index]
-#         end
-#     end
-# end
-
-def loop_alpha (char, num)
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    new_index = (alpha.index(char) + num) % 26
-    return alpha[new_index]
+def caesar_cipher (str, num)
+    newStr = ""
+    str.each_char do |char|
+        unless char == $alpha[char]
+            if char == $alpha.upcase[char]
+                newStr.insert(-1, loop_alpha(char.downcase, num).upcase)
+            else
+                 newStr.insert(-1, char)
+            end
+        else 
+            newStr.insert(-1, loop_alpha(char, num))
+        end
+    end
+    newStr
 end
 
-p loop_alpha("s", 8)
+
+def loop_alpha (char, num)
+    new_index = ($alpha.index(char) + num) % 26
+    $alpha[new_index]
+end
+
+p caesar_cipher("Let's a go!", 2)
+#p loop_alpha("s", 8)
